4e1802f9e519ac45ad314067c545b0051a609cef
siccoo <obichilaka@gmail.com>
2021-03-17 12:35:17 +0100

    complete set-up of the Nav bar & other dependencies installed

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -7,25 +7,37 @@ import NewsLogo from "../../images/NewsLogo.svg";
 const Topbar = () => {
     return (
         <div>
-             {/* NAVIGATION BAR STARTS */}
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="/">
-                    <img src={NewsLogo} alt="news-logo" className="news-logo" />
-                </a>
-                <form className="form-inline mx-auto">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                </form>
+            <section className="nav-section">
                 <div className="">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link className="nav-btn1" href="/">Documentation</Link>
-                        </li><br />
-                        <li className="nav-item">
-                            <Link className="nav-btn" href="/">Buy now</Link>
-                        </li><br />
-                    </ul>
+                    {/* NAVIGATION BAR STARTS */}
+                    <nav className="navbar navbar-expand-lg navbar-light nav-bg justify-content-sm-start fixed-top" >
+                    <Link to={'/'} className="navbar-brand order-1 order-lg-0 ml-lg-0 ml-2 mr-auto">
+                        <img src={NewsLogo} alt='news-logo' className="news-logo" />
+                    </Link>
+                    <button className="navbar-toggler align-self-start hidden-sm-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation" style={{marginTop: 5 + 'px'}}>
+                        <span className="line"></span> 
+                        <span className="line"></span> 
+                        <span className="line"></span>
+                    </button>
+                        <div id="navbarNavDropdown" className="navbar-collapse collapse">
+                            <form className="navbar form-inline mx-auto">
+                                <input className="form-control mr-sm-3" type="search" placeholder="Health Grant" />
+                            </form>
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-btn1" href="/">A</Link>
+                                </li><br />
+                                <li className="nav-item">
+                                    <Link className="nav-btn1" href="/">A</Link>
+                                </li><br />
+                                <li className="nav-item">
+                                    <Link className="nav-btn1" href="/">A</Link>
+                                </li><br />
+                            </ul>
+                        </div>
+                    </nav> 
                 </div>
-            </nav>
+            </section>
         </div>
     )
 }
