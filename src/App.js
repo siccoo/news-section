a7cc94ee50278fa3c49d8d868b8c21bb415c9fc2
siccoo <obichilaka@gmail.com>
2021-03-17 13:51:21 +0100

    creating the news modules

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 
 import Topbar from "./components/navbar/index";
+import News from "./pages/news/index";
 
 const App = () => {
   return (
     <Router>
       <Topbar />
         <Switch>
-          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/" component={News} />
         </Switch>
     </Router>
   );
--- src/pages/news/index.js
+++ src/pages/news/index.js
@@ -3,7 +3,21 @@ import React from 'react'
 const News = () => {
     return (
         <div>
-            
+            <section className="news">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-3">
+
+                        </div>
+                        <div className="col-md-6">
+
+                        </div>
+                        <div className="col-md-3">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
